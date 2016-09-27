b8334ce9334b23524c7d0e8d532c8e122dd6310e
David Gaynor <runnedrun@gmail.com>
2016-09-27 08:05:04 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,20 @@
+var quantityInput = $(".element-quantity-input")
+
+quantityInput.keydown(function(e) {
+  var keyCode = e.keyCode;
+  var inputValue = quantityInput.val();
+
+  // enter key!
+  if (keyCode == 13) {	
+    addElementsToThePage(inputValue);
+  }
+})
+
+function addElementsToThePage(numberOfElements) {	
+  $(".element-display").html("");
+
+  for (var i = 0; i < numberOfElements; i++) {
+    var newEl = $("<div class='new-el'>Hi there!</div>");		
+    $(".element-display").append(newEl);
+  }
+}
